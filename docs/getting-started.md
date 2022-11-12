afc65cc5e1fcab9530e61d1e15fcd09945bc375b
Ely Lucas <ely@meta-tek.net>
2022-11-12 13:55:19 -0700

    adding docusaurus

---
+++ docs/getting-started.md
@@ -0,0 +1,20 @@
+---
+slug: /
+---
+
+# Getting Started
+
+Welcome to the Workshop!
+
+This workshop covers cool stuff.
+
+To get started, clone the repo and install the dependencies:
+
+## Clone Repo and Install Dependencies
+
+
+## Start App
+
+
+## Get to Know the App
+
---
+++ docusaurus.config.js
@@ -0,0 +1,65 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'New Workshop site',
+  tagline: 'workshops are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+        sitemap: false,
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'My Workshop',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+      },      
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
